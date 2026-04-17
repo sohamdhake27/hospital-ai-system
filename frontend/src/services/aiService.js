96b69c63bdeb890083ff40a278a97c957c771d35
Soham Dhake <sohamdhake2762005@gmail.com>
2026-04-17 17:49:29 +0530

    fix aiService syntax error

--- frontend/src/services/aiService.js
+++ frontend/src/services/aiService.js
@@ -1,12 +1,15 @@
 export const predictRisk = async (data) => {
   try {
-    const response = await fetch("https://hospital-ai-system-3uda.onrender.com/api/ai/predict",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
+    const response = await fetch(
+      "https://hospital-ai-system-3uda.onrender.com/api/ai/predict",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      }
+    );
 
     return await response.json();
   } catch (error) {
