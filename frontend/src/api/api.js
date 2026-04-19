47d787d3b15e9bab0a139c5cbd75cba606c249b0
Soham Dhake <sohamdhake2762005@gmail.com>
2026-04-19 23:02:22 +0530

    fix env file

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export const API = axios.create({
-  baseURL: API_BASE_URL
+  baseURL: `${API_BASE_URL}/api`
 });
 
 API.interceptors.request.use((req) => {
